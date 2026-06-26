5f61817ccb03b61dcd95603e98f59cb466db60e8
Raulzito <vigorre.oficial@gmail.com>
2026-06-26 11:19:06 -0300

    Update next.config.ts

--- packages/frontend/web/next.config.ts
+++ packages/frontend/web/next.config.ts
@@ -1,1 +1,12 @@
+import type { NextConfig } from "next";
 
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+  output: "standalone",
+};
+
+export default nextConfig;
